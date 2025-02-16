edd0fd7c4ece67b2f181684c5eca6b6a33f10fee
Lucas Menezes <livedolux@gmail.com>
2025-02-16 17:29:59 -0300

    cria estrutura básica inicial do projeto

---
+++ NexusBlog/src/main/java/com/nexusBlog/NexusBlog/NexusBlogApplication.java
@@ -0,0 +1,13 @@
+package com.nexusBlog.NexusBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusBlogApplication.class, args);
+	}
+
+}
